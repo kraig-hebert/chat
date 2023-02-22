9e27f90d5a9bcb4cd3f4323a67c1f429fe242994
Kraig Hebert <kraighebert@gmail.com>
2023-02-21 20:58:53 -0500

    added search bar to MenuOptions component

--- client/src/components/sideMenu/menuOptions/menuSearchInput/MenuSearchInput.jsx
+++ client/src/components/sideMenu/menuOptions/menuSearchInput/MenuSearchInput.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import { IoSearchCircleOutline, IoSearchCircle } from 'react-icons/io5';
 import { useStyles } from './menuSearchInputStyles';
 
 const MenuSearchInput = (props) => {
   const {} = props;
   const classes = useStyles();
+  const [isFocused, setIsFocused] = useState(false);
+  const [inputValue, setInputValue] = useState('');
   return (
     <div className={classes.menuSearchInput}>
-      <span className={classes.inputPlaceholder}>Search</span>
-      <input type="text" className={classes.input} />
+      {isFocused ? (
+        <IoSearchCircle className={classes.activeSearchIcon} />
+      ) : (
+        <IoSearchCircleOutline className={classes.searchIcon} />
+      )}
+
+      <input
+        type="text"
+        className={classes.input}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      {!inputValue && (
+        <span className={classes.inputPlaceholder}>Search...</span>
+      )}
     </div>
   );
 };
--- client/src/components/sideMenu/menuOptions/menuSearchInput/menuSearchInputStyles.js
+++ client/src/components/sideMenu/menuOptions/menuSearchInput/menuSearchInputStyles.js
@@ -5,23 +5,44 @@ export const useStyles = createUseStyles((theme) => ({
     position: 'relative',
     width: '80%',
   },
-  inputPlaceholder: {
+  searchIcon: {
     position: 'absolute',
-    left: '10px',
+    left: '5px',
     top: '50%',
     transform: 'translateY(-50%)',
     color: theme.light.main,
-    fontSize: '.8rem',
+    fontSize: '1.25rem',
+    pointerEvents: 'none',
+  },
+  activeSearchIcon: {
+    extend: 'searchIcon',
+    color: theme.blue.main,
+  },
+  inputPlaceholder: {
+    transition: '.2s',
+    position: 'absolute',
+    left: '30px',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    color: theme.light.halfAlpha,
+    fontSize: '.9rem',
     pointerEvents: 'none',
   },
   input: {
     width: '100%',
-    height: '25px',
+    height: '30px',
+    paddingLeft: '30px',
     backgroundColor: theme.mainBG.main,
+    caretColor: theme.light.main,
+    color: theme.light.main,
     borderRadius: '5px',
     border: 'none',
     '&:active, &:focus': {
       outline: '2px solid ' + theme.blue.main,
+      '& + span': {
+        left: '50px',
+        color: theme.light.transparant,
+      },
     },
   },
 }));
