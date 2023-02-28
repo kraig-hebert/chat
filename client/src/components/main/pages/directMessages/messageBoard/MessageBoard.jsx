8a89d1548fe0fb0b89ae098b2ea2a2e206151a5d
Kraig Hebert <kraighebert@gmail.com>
2023-02-28 14:17:40 -0500

    fixed bug when selecting a new activeDirectMessageThread, where the scrollHeight wasn't being rerendered so the force scroll to bottom wasn't going all the way

--- client/src/components/main/pages/directMessages/messageBoard/MessageBoard.jsx
+++ client/src/components/main/pages/directMessages/messageBoard/MessageBoard.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import { selectActiveDirectMessageThread } from '../../../../../reducers/appSettings';
 import { useStyles } from './messageBoardStyles';
 import MessageBlock from './messageBlock/MessageBlock';
 
@@ -15,10 +17,12 @@ const MessageBoard = (props) => {
   const {} = props;
   const classes = useStyles();
   const containerRef = useRef();
+  const activeDirectMessageThead = useSelector(selectActiveDirectMessageThread);
 
   useEffect(() => {
     containerRef.current.scrollTop = containerRef.current.scrollHeight;
-  }, []);
+  }, [activeDirectMessageThead]);
+
   return (
     <div className={classes.messageBoardContainer} ref={containerRef}>
       <div className={classes.messageBoard}>{renderedMessageBlocks}</div>
