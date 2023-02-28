8500a65dcf0f034a96028bda34c629e3df83a37a
Kraig Hebert <kraighebert@gmail.com>
2023-02-28 14:13:43 -0500

    remove unecessary code

--- client/src/components/sideMenu/menuOptions/directMessagesOptions/DirectMessagesOptions.jsx
+++ client/src/components/sideMenu/menuOptions/directMessagesOptions/DirectMessagesOptions.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { directMessageThreadSelected } from '../../../../reducers/appSettings';
 import { useStyles } from './directMessagesOptionsStyles';
 import DirectMessageCard from './directMessageCard/DirectMessageCard';
 import MessageTypeSelector from './directMessageCard/messageTypeSelector/MessageTypeSelector';
@@ -13,7 +11,6 @@ import dummyData from './dummyData';
 const DirectMessagesOptions = (props) => {
   const {} = props;
   const classes = useStyles();
-  const dispatch = useDispatch();
 
   const [selectedType, setSelectedType] = useState('solo');
 
