382617cde578ffa7cd3ff68c425ec23e4ad0e836
Kraig Hebert <kraighebert@gmail.com>
2023-03-05 12:14:35 -0500

    updated logic in DirectMessageOptions to filter solo and group names

--- client/src/components/sideMenu/menuOptions/directMessagesOptions/DirectMessagesOptions.jsx
+++ client/src/components/sideMenu/menuOptions/directMessagesOptions/DirectMessagesOptions.jsx
@@ -6,7 +6,6 @@ import DirectMessageCard from './directMessageCard/DirectMessageCard';
 import MessageTypeSelector from './directMessageCard/messageTypeSelector/MessageTypeSelector';
 
 //import dummyData for cards
-import dummyData from './dummyData';
 import { users, groups } from '../../../../data/dummyData';
 
 const DirectMessagesOptions = (props) => {
@@ -16,27 +15,27 @@ const DirectMessagesOptions = (props) => {
   const [selectedType, setSelectedType] = useState('solo');
   const renderCards = () => {
     if (selectedType === 'solo') {
-      return users.map((user, index) => {
-        const data = {
-          user: user,
-          date: new Date(),
-        };
-        return <DirectMessageCard cardData={data} key={index} />;
-      });
+      return users
+        .filter((user) => user.startsWith(inputValue))
+        .map((user, index) => {
+          const data = {
+            user: user,
+            date: new Date(),
+          };
+          return <DirectMessageCard cardData={data} key={index} />;
+        });
     } else if (selectedType === 'group') {
-      return groups.map((group, index) => {
-        const data = {
-          user: group.title,
-          date: new Date(),
-        };
-        return <DirectMessageCard cardData={data} key={index} />;
-      });
+      return groups
+        .filter((group) => group.title.startsWith(inputValue))
+        .map((group, index) => {
+          const data = {
+            user: group.title,
+            date: new Date(),
+          };
+          return <DirectMessageCard cardData={data} key={index} />;
+        });
     }
   };
-
-  const renderedGroupCards = dummyData.group.map((message, index) => (
-    <DirectMessageCard message={message} key={index} />
-  ));
 
   return (
     <div className={classes.directMessagesOptions}>
